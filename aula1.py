6449b4243eea4f0f5e872ad430fd12b683ec62a3
quintanabock <oliviaquintanabock@gmail.com>
2024-10-22 10:35:24 +0100

    Adicionado o ficheiro aula1.py
    Foi adicionado via upload file

---
+++ aula1.py
@@ -0,0 +1,50 @@
+print("olá mundo!!!")
+
+numero1= 12
+numero2=5
+soma=numero1/numero2
+print("a soma de", numero1, "com", numero2,"é de", soma)
+
+numero1=7
+numero2=7
+soma=numero1*numero2
+print("a soma de",numero1, "com",numero2,"é de",soma)
+
+numero1=20
+numero2=30
+soma=numero1+numero2
+print("a soma de",numero1, "com",numero2,"é de",soma)
+
+numero1=5
+numero2=3
+soma=numero1-numero2
+print("a soma de",numero1, "com",numero2,"é de",soma)
+
+
+ovos=2
+esponjaLoica=1.50
+escovaDentes=4
+farinha=2
+acucar=2
+carne=5
+
+
+cestoCompras = ovos + esponjaLoica+escovaDentes + farinha+acucar+carne
+
+print("A compra foi de ", cestoCompras, "€")
+
+
+mesada=50
+
+verniz=3
+bodysplash =19
+capinhatelemovel=4
+blusa=3
+fones=20
+print("a compra foi de",cestoCompras,)
+
+
+
+
+
+
